12295b37cb11676cba2f8d4732bb55a8b929c72f
brian33xd <132594461+brian33xd@users.noreply.github.com>
2023-06-06 17:33:28 -0300

    Add files via upload

---
+++ Ejercicio2.js
@@ -0,0 +1,20 @@
+
+const nombre = "Brian";
+const fechaN= "28/07/2001";
+const edad= 21;
+const LibroFav = {
+    autor:"Antonio Santa Ana",
+    fecha: "13/03/1998", 
+    titulo: "Los ojos del perro siberiano",
+    url: "https://www.secst.cl/upfiles/documentos/01082018_1159am_5b61f508889a5.pdf"
+}
+if (true)
+{
+var SoyDesarrollador = true;
+console.log("Sí")
+}
+
+
+
+let Lista = [nombre, fechaN, edad,SoyDesarrollador, LibroFav]
+console.log(Lista)
