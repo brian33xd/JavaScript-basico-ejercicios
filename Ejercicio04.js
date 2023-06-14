4ac47a980f1af45662c32382f88529f6f8d2bebd
brian33xd <132594461+brian33xd@users.noreply.github.com>
2023-06-14 10:17:59 -0300

    Add files via upload

---
+++ Ejercicio04.js
@@ -0,0 +1,24 @@
+let nombre="Brian"
+
+let apellido="Godoy"
+
+let estudiante=`${nombre} ${apellido}`
+
+console.log(estudiante)
+
+let estudianteMayus= (estudiante.toUpperCase())
+
+let estudianteMinus= (estudiante.toLowerCase())
+
+console.log(estudianteMayus)
+
+console.log(estudianteMinus)
+
+var Letras= estudiante.length
+console.log(Letras)
+
+var Primera= nombre.charAt(0)
+var Ultima= apellido.charAt(4)
+
+console.log(Primera)
+console.log(Ultima)
