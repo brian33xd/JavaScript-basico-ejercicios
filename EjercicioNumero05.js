1b57c106e61c39390b3c8f0e9c6792a5065153be
brian33xd <132594461+brian33xd@users.noreply.github.com>
2023-06-14 20:14:23 -0300

    Add files via upload

---
+++ EjercicioNumero05.js
@@ -0,0 +1,16 @@
+//Ejercicio de curso de Javascript Básico número 05
+
+let altura= 175;
+let alturaD= 1.75;
+let peso = 64.40;
+let AltRedondeada= alturaD.toPrecision(2)
+
+console.log(AltRedondeada)
+
+let PesoRedondeado= peso.toFixed()
+
+console.log(PesoRedondeado)
+
+let Si = Number.MAX_VALUE + 1 ;
+
+console.log(Si)
